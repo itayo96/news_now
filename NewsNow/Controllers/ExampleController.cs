75c7043465fd59ccfca057e712cc3d0628e51deb
Itay Ovadia <itay.ovadi@gmail.com>
2018-09-17 22:16:32 +0300

    Added an example url route

---
+++ NewsNow/Controllers/ExampleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace NewsNow.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // 
+        // GET: /Example/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string AddEntersAmount(int amount = 1)
+        {
+            return "The cuurent enters amount is: " + amount;
+        }
+    }
+}
