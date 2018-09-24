7375c743a8fe9633b2c90ac04bb61a4fe8d8876f
Itay Ovadia <itay.ovadi@gmail.com>
2018-09-24 23:33:01 +0300

    Added a basic page that get some coordinates from the server and shows them on a bing map

---
+++ NewsNow/Controllers/ArticlesLocationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NewsNow.Models;
+
+namespace NewsNow.Controllers
+{
+    public class ArticlesLocationController : Controller
+    {
+        private readonly NewsNowContext _context;
+
+        public ArticlesLocationController(NewsNowContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetLocations()
+        {
+            var locations = new List<PointF>()
+            {
+                new PointF(12.505353f,55.335292f),
+                 new PointF(13.505353f,55.485292f),
+                  new PointF(13.655353f,55.665292f)
+            };
+            return Json(locations);
+        }
+    }
+}
