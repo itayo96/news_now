5f7987accbc5d1e040bdf451e497a917a13ac068
Omer Horev <omer.hv@gmail.com>
2018-09-24 18:15:30 +0300

    Minor design changes
    * Added nice buttons
    * Added summery and category badge to the article page

--- NewsNow/Controllers/ArticlesController.cs
+++ NewsNow/Controllers/ArticlesController.cs
@@ -37,8 +37,9 @@ namespace NewsNow.Controllers
                 return NotFound();
             }
 
-            var article = await _context.Articles
+            var article = await _context.Articles.Include(a => a.Category)
                 .FirstOrDefaultAsync(m => m.ArticleId == id);
+
             if (article == null)
             {
                 return NotFound();
