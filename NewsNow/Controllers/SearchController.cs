466788baafbb2ce9028f46bcdd67c5e0021e7b0d
Itay Ovadia <itay.ovadi@gmail.com>
2018-09-27 16:20:44 +0300

    Added a simple search page that search by categories

---
+++ NewsNow/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NewsNow.Models;
+
+namespace NewsNow.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly NewsNowContext _context;
+
+        public SearchController(NewsNowContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Articles
+        public async Task<IActionResult> Index(string categoryId)
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "CategoryId", "Name", categoryId);    // preselect item in selectlist by categoryId param
+
+            if (String.IsNullOrWhiteSpace(categoryId))
+            {
+                return View();
+            }
+
+            var result = _context.Articles.Where(x => x.CategoryId == Int32.Parse(categoryId)).Take(50);
+
+            return View(result);
+        }
+    }
+}
