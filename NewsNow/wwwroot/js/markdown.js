b854877fdd185b5dde8a3e42709c587fb84a4731
Omer Horev <omer.hv@gmail.com>
2018-10-01 11:51:16 +0300

    Added video support
    - Added SimpleMDE (markdown editor) support for new video tag (syntax highlight)
    - Added Showdown (markdown render) support for new video tag (rendered as html5 video and responsive bootstrap)

---
+++ NewsNow/wwwroot/js/markdown.js
@@ -0,0 +1,41 @@
+﻿var showdown_converters = function (converter) {
+    return [
+        {
+            type: 'html',
+            regex: '<img src=(.*)>',
+            replace: '<img class="img-fluid img-thumbnail" src=$1>'
+        },
+        {
+            type: 'lang',
+            regex: /\!\[VIDEO\]\(.*\)/,
+            replace: function (match, converter, options) {
+                var url = match.slice(9, -1)
+
+                return '<div class="embed-responsive embed-responsive-16by9"><video controls><source src="' + url + '"></video></div>'
+
+            }
+        }
+
+        //<video width="320" height="240" controls><source src="movie.mp4" type="video/mp4"></video>
+    ]
+}
+
+var make_markdown_editor = function (e, initial_content) {
+    var simplemde = new SimpleMDE({ element: e, insertTexts: { video: ["![VIDEO](", ")"] } });
+    var str = decodeHtml(initial_content)
+    simplemde.value(str);
+}
+
+var markdown_to_html = function (markdown_string) {
+    var converter = new showdown.Converter({ extensions: [showdown_converters] }),
+        text = decodeHtml(markdown_string),
+        html = converter.makeHtml(text);
+
+    return html;
+}
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
