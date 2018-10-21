71631579acffe9dbc0e3ae0b4e50a8ad8af369ee
Netay <netay1234@gmail.com>
2018-10-21 23:55:59 +0300

    Initial work on Users

---
+++ NewsNow/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewsNow.Models;
+
+namespace NewsNow.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly NewsNowContext _context;
+
+        public UsersController(NewsNowContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,Username,Password,Permissions")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+    }
+}
---
+++ NewsNow/Migrations/20180920103420_Initial.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsNow.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 600, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 4,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 5,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 6,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 7,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 8,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 9,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 20, 13, 34, 20, 602, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 879, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 4,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 5,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 6,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 7,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 8,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 9,
+                column: "DateCreated",
+                value: new DateTime(2018, 9, 19, 21, 18, 57, 880, DateTimeKind.Local));
+        }
+    }
+}
