e836588bc80b23e757f24c1f8c89f7fa86e95ecc
Unknown <yotamamir96@gmail.com>
2018-10-27 17:15:07 +0300

    Fixed category edit submission

--- NewsNow/Controllers/CategoriesController.cs
+++ NewsNow/Controllers/CategoriesController.cs
@@ -88,7 +88,7 @@ namespace NewsNow.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Category categoryModel)
+        public async Task<IActionResult> Edit(int id, [Bind("CategoryId, Name, Description, Color")] Category categoryModel)
         {
             if (id != categoryModel.CategoryId)
             {
