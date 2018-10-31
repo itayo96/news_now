7764cab4450e008b92562ac30f1499bfd452f351
Omer Horev <omer.hv@gmail.com>
2018-10-31 16:34:45 +0200

    Added tweeter to the markdown
    Added the tweeter API to the markdown engine using the ![TWEETER] tag

--- NewsNow/wwwroot/js/markdown.js
+++ NewsNow/wwwroot/js/markdown.js
@@ -14,14 +14,27 @@
                 return '<div class="embed-responsive embed-responsive-16by9"><video controls><source src="' + url + '"></video></div>'
 
             }
-        }
+        },
+        {
+            type: 'lang',
+            regex: /\!\[TWITTER\]\(.*\)/,
+            replace: function (match, converter, options) {
+                var url = match.slice(11, -1)
 
-        //<video width="320" height="240" controls><source src="movie.mp4" type="video/mp4"></video>
+                return "<div class='tweeter_tweet col-12' tweeter-url='https://twitter.com/Interior/status/463440424141459456'><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script></div>"
+            }
+        }
     ]
 }
 
 var make_markdown_editor = function (e, initial_content) {
-    var simplemde = new SimpleMDE({ element: e, insertTexts: { video: ["![VIDEO](", ")"] } });
+    var simplemde = new SimpleMDE({
+        element: e,
+        insertTexts: {
+            video: ["![VIDEO](", ")"],
+           video: ["![TWITTER](", ")"]
+        }
+    });
     var str = decodeHtml(initial_content)
     simplemde.value(str);
 }
@@ -34,8 +47,30 @@ var markdown_to_html = function (markdown_string) {
     return html;
 }
 
+var load_markdown_tweeter = function () {
+    $(".tweeter_tweet").each(function (index) {
+        console.log(index + ":" + $(this).attr("tweeter-url"));
+        load_tweet($(this))
+    });
+}
+
 function decodeHtml(html) {
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
     return txt.value;
 }
+
+var load_tweet = function (obj) {
+    console.log(obj)
+    console.log(obj.attr("tweeter-url"))
+    $.ajax({
+        type: "GET",
+        url: "https://publish.twitter.com/oembed?url=" + obj.attr("tweeter-url"),
+        data: "https://twitter.com/Interior/status/463440424141459456",
+        dataType: 'jsonp',
+        crossDomain: true,
+        success: function (data) {
+            obj.append(data['html']);
+        }
+    })
+};
