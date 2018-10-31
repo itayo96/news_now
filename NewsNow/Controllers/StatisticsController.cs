d0ba3382e7e4d55a452e4b2b14ff0b652b64afe6
Unknown <yotamamir96@gmail.com>
2018-11-01 00:13:04 +0200

    The comments JSON is now requested from the server

--- NewsNow/Controllers/StatisticsController.cs
+++ NewsNow/Controllers/StatisticsController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NewsNow.Models;
+using Newtonsoft.Json;
 
 namespace NewsNow.Controllers
 {
@@ -25,129 +27,148 @@ namespace NewsNow.Controllers
             return View();
         }
 
-    //    // GET: Statistics/Details/5
-    //    public async Task<IActionResult> Details(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return NotFound();
-    //        }
+        public async Task<IActionResult> CommentsStatistics()
+        {
+            string jsonData = null;
 
-    //        var statistics = await _context.ExampleStatistics
-    //            .FirstOrDefaultAsync(m => m.ID == id);
-    //        if (statistics == null)
-    //        {
-    //            return NotFound();
-    //        }
+            using (StreamReader r = new StreamReader("wwwroot/assets/comments-statistics.json"))
+            {
+                jsonData = r.ReadToEnd();
+            }
 
-    //        return View(statistics);
-    //    }
+            dynamic deserializedJson = JsonConvert.DeserializeObject(jsonData);
 
-    //    // GET: Statistics/Create
-    //    public IActionResult Create()
-    //    {
-    //        return View();
-    //    }
+            if (deserializedJson == null)
+            {
+                return NotFound();
+            }
 
-    //    // POST: Statistics/Create
-    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<IActionResult> Create([Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-    //            _context.Add(statistics);
-    //            await _context.SaveChangesAsync();
-    //            return RedirectToAction(nameof(Index));
-    //        }
-    //        return View(statistics);
-    //    }
+            return Json(deserializedJson);
+        }
 
-    //    // GET: Statistics/Edit/5
-    //    public async Task<IActionResult> Edit(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return NotFound();
-    //        }
+        //    // GET: Statistics/Details/5
+        //    public async Task<IActionResult> Details(int? id)
+        //    {
+        //        if (id == null)
+        //        {
+        //            return NotFound();
+        //        }
 
-    //        var statistics = await _context.ExampleStatistics.FindAsync(id);
-    //        if (statistics == null)
-    //        {
-    //            return NotFound();
-    //        }
-    //        return View(statistics);
-    //    }
+        //        var statistics = await _context.ExampleStatistics
+        //            .FirstOrDefaultAsync(m => m.ID == id);
+        //        if (statistics == null)
+        //        {
+        //            return NotFound();
+        //        }
 
-    //    // POST: Statistics/Edit/5
-    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    //    [HttpPost]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<IActionResult> Edit(int id, [Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
-    //    {
-    //        if (id != statistics.ID)
-    //        {
-    //            return NotFound();
-    //        }
+        //        return View(statistics);
+        //    }
 
-    //        if (ModelState.IsValid)
-    //        {
-    //            try
-    //            {
-    //                _context.Update(statistics);
-    //                await _context.SaveChangesAsync();
-    //            }
-    //            catch (DbUpdateConcurrencyException)
-    //            {
-    //                if (!StatisticsExists(statistics.ID))
-    //                {
-    //                    return NotFound();
-    //                }
-    //                else
-    //                {
-    //                    throw;
-    //                }
-    //            }
-    //            return RedirectToAction(nameof(Index));
-    //        }
-    //        return View(statistics);
-    //    }
+        //    // GET: Statistics/Create
+        //    public IActionResult Create()
+        //    {
+        //        return View();
+        //    }
 
-    //    // GET: Statistics/Delete/5
-    //    public async Task<IActionResult> Delete(int? id)
-    //    {
-    //        if (id == null)
-    //        {
-    //            return NotFound();
-    //        }
+        //    // POST: Statistics/Create
+        //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> Create([Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            _context.Add(statistics);
+        //            await _context.SaveChangesAsync();
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        return View(statistics);
+        //    }
 
-    //        var statistics = await _context.ExampleStatistics
-    //            .FirstOrDefaultAsync(m => m.ID == id);
-    //        if (statistics == null)
-    //        {
-    //            return NotFound();
-    //        }
+        //    // GET: Statistics/Edit/5
+        //    public async Task<IActionResult> Edit(int? id)
+        //    {
+        //        if (id == null)
+        //        {
+        //            return NotFound();
+        //        }
 
-    //        return View(statistics);
-    //    }
+        //        var statistics = await _context.ExampleStatistics.FindAsync(id);
+        //        if (statistics == null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        return View(statistics);
+        //    }
 
-    //    // POST: Statistics/Delete/5
-    //    [HttpPost, ActionName("Delete")]
-    //    [ValidateAntiForgeryToken]
-    //    public async Task<IActionResult> DeleteConfirmed(int id)
-    //    {
-    //        var statistics = await _context.ExampleStatistics.FindAsync(id);
-    //        _context.ExampleStatistics.Remove(statistics);
-    //        await _context.SaveChangesAsync();
-    //        return RedirectToAction(nameof(Index));
-    //    }
+        //    // POST: Statistics/Edit/5
+        //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> Edit(int id, [Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
+        //    {
+        //        if (id != statistics.ID)
+        //        {
+        //            return NotFound();
+        //        }
 
-    //    private bool StatisticsExists(int id)
-    //    {
-    //        return _context.ExampleStatistics.Any(e => e.ID == id);
-    //    }
+        //        if (ModelState.IsValid)
+        //        {
+        //            try
+        //            {
+        //                _context.Update(statistics);
+        //                await _context.SaveChangesAsync();
+        //            }
+        //            catch (DbUpdateConcurrencyException)
+        //            {
+        //                if (!StatisticsExists(statistics.ID))
+        //                {
+        //                    return NotFound();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        return View(statistics);
+        //    }
+
+        //    // GET: Statistics/Delete/5
+        //    public async Task<IActionResult> Delete(int? id)
+        //    {
+        //        if (id == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        var statistics = await _context.ExampleStatistics
+        //            .FirstOrDefaultAsync(m => m.ID == id);
+        //        if (statistics == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        return View(statistics);
+        //    }
+
+        //    // POST: Statistics/Delete/5
+        //    [HttpPost, ActionName("Delete")]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> DeleteConfirmed(int id)
+        //    {
+        //        var statistics = await _context.ExampleStatistics.FindAsync(id);
+        //        _context.ExampleStatistics.Remove(statistics);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    private bool StatisticsExists(int id)
+        //    {
+        //        return _context.ExampleStatistics.Any(e => e.ID == id);
+        //    }
     }
 }
--- NewsNow/wwwroot/js/statistics.js
+++ NewsNow/wwwroot/js/statistics.js
@@ -55,6 +55,6 @@ function draw(data) {
         .attr("height", function (d, i, j) { return height - y0(d.comments); });
 }
 
-d3.json('assets/comments-statistics.json', function(data) {
+$.getJSON("/Statistics/CommentsStatistics", null, function (data) {
     draw(data);
 });
