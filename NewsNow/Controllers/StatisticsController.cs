51d9b695a0e1327deb154d7098a3e2d67a76e21f
Yotam Amir <yotamamir96@gmail.com>
2018-11-01 23:25:39 +0200

    Now working with a json from the server for articles

--- NewsNow/Controllers/StatisticsController.cs
+++ NewsNow/Controllers/StatisticsController.cs
@@ -40,129 +40,17 @@ namespace NewsNow.Controllers
             return Json(statistics);
         }
 
-        //    // GET: Statistics/Details/5
-        //    public async Task<IActionResult> Details(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return NotFound();
-        //        }
+        public async Task<IActionResult> Articles()
+        {
+            var articlesPerCategory = await _context.Articles.GroupBy(x => x.CategoryId).ToListAsync();
 
-        //        var statistics = await _context.ExampleStatistics
-        //            .FirstOrDefaultAsync(m => m.ID == id);
-        //        if (statistics == null)
-        //        {
-        //            return NotFound();
-        //        }
+            var statistics = articlesPerCategory.Select(categoryArticles => new
+            {
+                label = categoryArticles.First().CategoryId,
+                value = categoryArticles.Count()
+            }).ToList();
 
-        //        return View(statistics);
-        //    }
-
-        //    // GET: Statistics/Create
-        //    public IActionResult Create()
-        //    {
-        //        return View();
-        //    }
-
-        //    // POST: Statistics/Create
-        //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> Create([Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            _context.Add(statistics);
-        //            await _context.SaveChangesAsync();
-        //            return RedirectToAction(nameof(Index));
-        //        }
-        //        return View(statistics);
-        //    }
-
-        //    // GET: Statistics/Edit/5
-        //    public async Task<IActionResult> Edit(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var statistics = await _context.ExampleStatistics.FindAsync(id);
-        //        if (statistics == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        return View(statistics);
-        //    }
-
-        //    // POST: Statistics/Edit/5
-        //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> Edit(int id, [Bind("ID,EntersAmount,OldestManName")] ExampleStatisticsModel statistics)
-        //    {
-        //        if (id != statistics.ID)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        if (ModelState.IsValid)
-        //        {
-        //            try
-        //            {
-        //                _context.Update(statistics);
-        //                await _context.SaveChangesAsync();
-        //            }
-        //            catch (DbUpdateConcurrencyException)
-        //            {
-        //                if (!StatisticsExists(statistics.ID))
-        //                {
-        //                    return NotFound();
-        //                }
-        //                else
-        //                {
-        //                    throw;
-        //                }
-        //            }
-        //            return RedirectToAction(nameof(Index));
-        //        }
-        //        return View(statistics);
-        //    }
-
-        //    // GET: Statistics/Delete/5
-        //    public async Task<IActionResult> Delete(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var statistics = await _context.ExampleStatistics
-        //            .FirstOrDefaultAsync(m => m.ID == id);
-        //        if (statistics == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return View(statistics);
-        //    }
-
-        //    // POST: Statistics/Delete/5
-        //    [HttpPost, ActionName("Delete")]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> DeleteConfirmed(int id)
-        //    {
-        //        var statistics = await _context.ExampleStatistics.FindAsync(id);
-        //        _context.ExampleStatistics.Remove(statistics);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    private bool StatisticsExists(int id)
-        //    {
-        //        return _context.ExampleStatistics.Any(e => e.ID == id);
-        //    }
+            return Json(statistics);
+        }
     }
 }
