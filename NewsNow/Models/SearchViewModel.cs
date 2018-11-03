99983295d75f3acee48b993aa818212d22aad197
0xNetay <netay1234@gmail.com>
2018-11-03 16:57:25 +0200

    Some things i tried to make 2 models work together

---
+++ NewsNow/Models/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsNow.Models
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Article> allArticles { get; set; }
+        public IEnumerable<Comment> allComments { get; set; }
+
+    }
+}
